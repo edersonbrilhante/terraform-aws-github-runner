1b9efe873a7ebf7224c51f0794acc0454e58a10a
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-12-04 14:45:34 +0100

    feat: add support to use custom scale errors

--- lambdas/functions/control-plane/src/scale-runners/scale-up.ts
+++ lambdas/functions/control-plane/src/scale-runners/scale-up.ts
@@ -64,7 +64,7 @@ interface CreateEC2RunnerConfig {
   amiIdSsmParameterName?: string;
   tracingEnabled?: boolean;
   onDemandFailoverOnError?: string[];
-  scaleErrors: string[];
+  customScaleErrors?: string[];
 }
 
 function generateRunnerServiceConfig(githubRunnerConfig: CreateGitHubRunnerConfig, token: string) {
@@ -483,7 +483,7 @@ export async function scaleUp(payloads: ActionRequestMessageSQS[]): Promise<stri
         amiIdSsmParameterName,
         tracingEnabled,
         onDemandFailoverOnError,
-        scaleErrors,
+        customScaleErrors,
       },
       newRunners,
       githubInstallationClient,
