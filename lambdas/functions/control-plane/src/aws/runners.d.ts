e90caa80cb4be9a00e8dce45f953d91d820aa822
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-12-18 23:11:16 +0100

    refactor: remove custom friom variable and add default values

--- lambdas/functions/control-plane/src/aws/runners.d.ts
+++ lambdas/functions/control-plane/src/aws/runners.d.ts
@@ -47,5 +47,5 @@ export interface RunnerInputParameters {
   amiIdSsmParameterName?: string;
   tracingEnabled?: boolean;
   onDemandFailoverOnError?: string[];
-  customScaleErrors?: string[];
+  scaleErrors: string[];
 }
--- lambdas/functions/control-plane/src/scale-runners/scale-up.ts
+++ lambdas/functions/control-plane/src/scale-runners/scale-up.ts
@@ -66,7 +66,7 @@ interface CreateEC2RunnerConfig {
   amiIdSsmParameterName?: string;
   tracingEnabled?: boolean;
   onDemandFailoverOnError?: string[];
-  customScaleErrors?: string[];
+  scaleErrors: string[];
 }
 
 function generateRunnerServiceConfig(githubRunnerConfig: CreateGitHubRunnerConfig, token: string) {
@@ -507,7 +507,7 @@ export async function scaleUp(payloads: ActionRequestMessageSQS[]): Promise<stri
         amiIdSsmParameterName,
         tracingEnabled,
         onDemandFailoverOnError,
-        customScaleErrors,
+        scaleErrors,
       },
       newRunners,
       githubInstallationClient,
